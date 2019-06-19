a71c239b9363cfd55c52d63f11a8b2dccb61f4a0
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2019-06-18 20:54:04 -0400

    Default app

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'nw-angular';
+
+  versions = '' +
+    'You are running NW.js (v' + window.nw.process.versions.nw + '), ' +
+    'Node.js (v' + window.nw.process.versions.node + '), ' +
+    'Chromium (v' + window.nw.process.versions.chromium + '), ' +
+    'and Angular (' + window.nw.App.manifest.dependencies['@angular/core'] + ').';
+
+  public links: any;
+
+  constructor() {
+    this.links = [
+      {
+        title: 'Angular Tutorial',
+        url: 'https://angular.io/tutorial'
+      },
+      {
+        title: 'Angular CLI Documentation',
+        url: 'https://angular.io/cli'
+      },
+      {
+        title: 'NW.js Documentation',
+        url: 'https://nwjs.io'
+      }
+    ]
+  }
+
+  public open(evt, link) {
+    evt.preventDefault();
+    window.nw.Shell.openExternal(link.url);
+  }
+}
