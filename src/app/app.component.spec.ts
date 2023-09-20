2af4513e508fbd33ab15cc3f5f23f0a52ca77807
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-09-20 16:21:19 -0400

    test: esm import jasmine

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import 'jasmine';
+import {} from 'jasmine';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
