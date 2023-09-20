1af7697733c9a160c8fdc05bb5c0e2ce1798627c
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-09-20 15:57:01 -0400

    chore: use jasmine without globals

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import * as jasmine from "jasmine-core";
+
+const {
+  beforeEach,
+  describe,
+  expect,
+  it,
+} = jasmine.NoGlobals();
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
