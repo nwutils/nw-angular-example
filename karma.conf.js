ec3429a6103debc0f7d58cf2953ff65acf124896
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-04-27 20:35:19 +0530

    chore(test): use headless

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,7 @@ module.exports = async function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: true
   });
 };
