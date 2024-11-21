f3ad9e35993be71221e85f32ab581943ad13b21c
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-11-21 18:28:59 +0530

    chore: remove AppComponent from NgModule declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
     AppRoutingModule
