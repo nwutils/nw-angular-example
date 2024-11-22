851d41a0c623a789dd8eecd52f9d8d18611f8060
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-11-22 17:50:36 +0530

    chore(ng): run standalone components migration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserModule, RouterModule, AppRoutingModule)]
+})
   .catch(err => console.error(err));
 
