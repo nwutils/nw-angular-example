1d97a66fb2186c8359011c41ac8834f60386d077
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-10-05 16:35:37 +0530

    chore: init ng v20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,12 @@
 import { Component } from '@angular/core';
-
-import nodeManifest from '../../package.json';
-
-type LinkType = {
-  title: string,
-  url: string,
-};
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: false,
+  styleUrl: './app.component.css'
 })
-
 export class AppComponent {
-  title = 'nw-angular-example';
-
-  versions = '' +
-  'You are running NW.js (v' + window?.nw?.process?.versions?.nw + ' ' + window?.nw?.process?.versions['nw-flavor'] + '), ' +
-  'Node.js (v' + window?.nw?.process?.versions?.node + '), ' +
-  'Chromium (v' + window?.nw?.process?.versions?.chromium + '), ' +
-  'and Angular (v' + nodeManifest.version + ').';
-
-  public links: LinkType[];
-
-  constructor() {
-    this.links = [
-      {
-        title: 'Angular Tutorial',
-        url: 'https://angular.io/tutorial'
-      },
-      {
-        title: 'Angular CLI Documentation',
-        url: 'https://angular.io/cli'
-      },
-      {
-        title: 'NW.js Documentation',
-        url: 'https://nwjs.io'
-      }
-    ];
-  }
-
-  // public open(event: Event, link: LinkType) {
-  //   event.preventDefault();
-  //   window.nw.Shell.openExternal(link.url);
-  // }
-
-  // public openDevTools(event: Event) {
-  //   event.preventDefault();
-  //   window.nw.Window.get().showDevTools();
-  // }
+  title = 'app';
 }
