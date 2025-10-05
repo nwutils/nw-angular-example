609ed1f5a16078382abe1e2cfff280bc15dffb66
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-10-05 18:32:48 +0530

    chore(deps): Angular v20, NW.js, v0.104.0, Node.js v24.7.0 (#269)
    Refs: #267

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,29 @@
-import {} from 'jasmine';
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nw-angular-example'`, () => {
+  it(`should have the 'app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('nw-angular-example');
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('app');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app');
   });
 });
